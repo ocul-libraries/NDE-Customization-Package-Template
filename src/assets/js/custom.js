292bb1e8b453cdb3a2231bbb65a19efba408b51e
Adam Doan <doana@uoguelph.ca>
2026-03-18 09:12:36 -0400

    Add example GTM code in custom.js

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,1 +1,16 @@
-console.log('Hello from custom.js');
+// TODO: Update G-YOURTAG and then remove the comments to make this active on go live. 
+/********** START GOOGLE ANALYTICS **********/
+/*
+var googleAnalyticsUrl = document.createElement('script');
+googleAnalyticsUrl.src = "https://www.googletagmanager.com/gtag/js?id=G-YOURTAG";
+googleAnalyticsUrl.type = 'text/javascript'; 
+googleAnalyticsUrl.async = true;
+document.head.appendChild(googleAnalyticsUrl); 
+var googleAnalyticsCode = document.createElement('script'); 
+googleAnalyticsCode.innerHTML = `window.dataLayer = window.dataLayer || []; 
+function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date()); 
+gtag('config', 'G-YOURTAG');`;
+document.head.appendChild(googleAnalyticsCode);
+*/  
+/********** END GOOGLE ANALYTICS **********/
